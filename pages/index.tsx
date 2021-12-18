e321d274d68adbe94a4fabfd7a69bf22b719ca24
bnt0106 <1952222@gm.uit.edu.vn>
2021-12-18 17:01:53 +0700

    init project

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import { Navbar, Footer } from '@components/common'
+
+
+const Home: NextPage = () => {
+  return (
+    
+        
+        <Navbar />
+      
+        
+    
+    
+  )
+}
+
+export default Home
